5005661a4fe534f841dadf36f4fe71ddce716f38
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-17 17:10:18 -0300

    feat(settings): created and configured the Django project.
    - apicine project created
    - altered settings.py in module
    - settings divided by environment

---
+++ apicine/settings/development.py
@@ -0,0 +1,23 @@
+from .base import *
+from decouple import config
+
+"""
+Configurações específicas para o ambiente de desenvolvimento.
+Specific settings for the development environment.
+
+"""
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
---
+++ apicine/settings/production.py
@@ -0,0 +1,23 @@
+from .base import *
+from decouple import config
+
+"""
+Configurações específicas para o ambiente de produção.
+Specific configurations for the production environment
+
+"""
+
+DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE' , default ='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
