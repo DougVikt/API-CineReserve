6df26dc2e42ed5f57d695f4beadd04c3b8a66582
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-18 17:22:08 -0300

    feat(theater): Add and register models for theater
    - add models:
       - room and seat

--- theater/models.py
+++ theater/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+
+class Room (models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    
+    class Meta: 
+        verbose_name = "Sala"
+        verbose_name_plural = "Salas"
+        
+    def __str__(self):
+        return self.name
+    
+
+
+class Seat(models.Model):
+    room = models.ForeignKey(Room , on_delete=models.CASCADE , related_name='seats')
+    row = models.CharField(max_length=5)
+    number = models.IntegerField()
+    
+    class Meta:
+        verbose_name = "Assento"
+        verbose_name_plural = "Assentos"
+        unique_together = ('room' , 'row' ,'number')
+        
+    def __str__(self):
+        return f"{self.row}{self.number} = {self.room}"
+    
