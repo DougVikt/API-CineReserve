82908dd655681423fedc73917209cb5fc2f758ec
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-18 17:18:28 -0300

    feat(accounts) : Add and register model for users

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# customized standard model
+class User(AbstractUser):
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    def __str__(self):
+        return self.username
+
+  
