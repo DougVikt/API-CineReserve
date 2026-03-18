58793a617c7901d70ab9b192b9a650bcd62c8f08
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-18 17:20:58 -0300

    feat(screening): Add and register models for screeningfor
    - add models :
       - genre , movie

--- screening/admin.py
+++ screening/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from .models import Genre , Movie
 
 
+admin.site.register(Genre)
+admin.site.register(Movie)
 
 
--- screening/models.py
+++ screening/models.py
@@ -1,1 +1,49 @@
 from django.db import models
+from django.utils import timezone 
+# extra imports 
+from datetime import timedelta
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30 , unique=True)
+    
+    class Meta:
+        verbose_name = "Genero"
+        verbose_name_plural = "Generos"
+        
+    def __str__(self):
+        return self.name
+    
+
+
+class Movie(models.Model):
+    
+    class AudioType(models.TextChoices):
+        DUBBED = 'dubbed' , 'Dublado'
+        SUBTITLED = 'subtitled' , 'Legendado'
+        
+    class AgeRating(models.TextChoices):
+        FREE = 'L' , 'Livre'
+        TEN = '10' , '10 anos'
+        TWELVE = '12' , '12 anos'
+        FOURTEEN = '14' , '14 anos'
+        SIXTEEN = '16' ,'16 anos'
+        EIGHTEEN = '18' , '18 anos'
+        
+    title = models.CharField( max_length=200)
+    description = models.TextField()
+    banner = models.ImageField(upload_to='banners/')
+    duration = models.IntegerField(help_text='Duração em minutos')
+    age_rating = models.CharField(max_length=3 , choices=AgeRating.choices)
+    audio_type = models.CharField( max_length=10 ,choices=AudioType.choices)
+    genres = models.ManyToManyField(Genre)
+    
+    class Meta:
+        verbose_name = 'Filme'
+        verbose_name_plural = 'Filmes'
+        
+    def __str__(self):
+        return f'{self.title} = {self.audio_type}'
+        
+        
+
