3698f727e9a1a296409f13f9eec890304270fccd
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-19 16:25:35 -0300

    feat(theater): add and register models for  screening
    - add models:
       - session , ticket , seatreservation

--- screening/admin.py
+++ screening/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Genre , Movie
+from screening.models import *
 
 
 admin.site.register(Genre)
 admin.site.register(Movie)
+admin.site.register(Session)
+admin.site.register(Ticket)
+admin.site.register(SeatReservation)
+
 
 
--- screening/models.py
+++ screening/models.py
@@ -45,5 +45,64 @@ class Movie(models.Model):
     def __str__(self):
         return f'{self.title} = {self.audio_type}'
         
-        
 
+class Session(models.Model):
+    movie = models.ForeignKey("Movie", on_delete=models.CASCADE , related_name='sessions')
+    room = models.ForeignKey("theater.Room", on_delete=models.CASCADE , related_name='sessions')
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time =models.TimeField(blank=True)
+    
+    def save(self, *args ,**kwargs):
+        if self.start_time and self.movie :
+            start = timezone.datetime.combine(self.date , self.start_time)
+            end = start + timedelta(minutes=self.movie.duration + 15)
+            self.end_time = end.time()
+        super().save(*args ,**kwargs )
+    
+    class Meta:
+        verbose_name = 'Sessão'
+        verbose_name_plural = 'Sessões'
+            
+            
+    def __str__(self):
+        return f'{self.movie.title} - {self.date}, {self.start_time}'
+    
+    
+    
+class Ticket(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='tickets')
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='tickets')
+    seat = models.ForeignKey('theater.Seat', on_delete=models.CASCADE, related_name='tickets')
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Ingresso'
+        verbose_name_plural = 'Ingressos'
+        unique_together = ('session', 'seat')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.session} - {self.seat}'
+    
+    
+class SeatReservation(models.Model):
+    class Status(models.TextChoices):
+        AVAILABLE = 'available' , 'Disponivel'
+        RESERVED = 'reserved' , 'Reservado'
+        PURCHARSED = 'purchased' , 'Comprado'
+        
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='reservations')
+    seat = models.ForeignKey("theater.Seat", on_delete=models.CASCADE , related_name='reservations')
+    user = models.ForeignKey("accounts.User", on_delete=models.SET_NULL ,null=True , blank=True ,related_name='reservations')
+    status = models.CharField(max_length=50 ,choices=Status.choices,default=Status.AVAILABLE )
+    expires_at = models.DateField(null=True ,blank=True)
+    
+    class Meta:
+        verbose_name = 'Reserva'
+        verbose_name_plural = 'Reservas'
+        unique_together = ('session' , 'seat')
+        
+    def __str__(self):
+        return f"{self.seat} - {self.session} - {self.status}"
+    
+        
