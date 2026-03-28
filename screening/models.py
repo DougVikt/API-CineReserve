86928069dfa1556acc0d3a2724bce171954000cd
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-03-28 15:00:46 -0300

    feat(db): Add a price column to the model session.

--- screening/models.py
+++ screening/models.py
@@ -50,6 +50,7 @@ class Session(models.Model):
     movie = models.ForeignKey("Movie", on_delete=models.CASCADE , related_name='sessions')
     room = models.ForeignKey("theater.Room", on_delete=models.CASCADE , related_name='sessions')
     date = models.DateField()
+    price = models.DecimalField( max_digits=6, decimal_places=2)
     start_time = models.TimeField()
     end_time =models.TimeField(blank=True)
     
